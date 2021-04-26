f683a19c41ba1cb5d8bd792d85cc43de8f40f91a
Garner Wong <jnwang.321@163.com>
2021-04-27 07:55:58 +0800

    .

---
+++ knowledge/100相同的树.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+"""
+给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+示例 1：
+输入：p = [1,2,3], q = [1,2,3]
+输出：true
+
+示例 2：
+输入：p = [1,2], q = [1,null,2]
+输出：false
+
+示例 3：
+输入：p = [1,2,1], q = [1,1,2]
+输出：false
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ knowledge/83删除排序链表中的重复元素.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+"""
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+返回同样按升序排列的结果链表。
+
+示例 1：
+输入：head = [1,1,2]
+输出：[1,2]
+
+示例 2：
+输入：head = [1,1,2,3,3]
+输出：[1,2,3]
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pass
+
+
+if __name__ == '__main__':
+    pass
