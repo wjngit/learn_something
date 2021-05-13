0f3d800056039166f8a74683316895049506a2d0
Garner Wong <jnwang.321@163.com>
2021-05-13 08:02:19 +0800

    .

--- algorithm/medium/3无重复字符的最长子串.py
+++ algorithm/medium/3无重复字符的最长子串.py
@@ -27,11 +27,39 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
+        滑动窗口：窗口内没有重复字母时，调整右边界；出现重复字母时，调整左边界。
         :type s: str
         :rtype: int
         """
-        pass
+        # 哈希集合，记录每个字符是否出现过
+        occ = set()
+        n = len(s)
+        # 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        rk, ans = -1, 0
+        for i in range(n):
+            if i != 0:
+                # 左指针向右移动一格，移除一个字符
+                occ.remove(s[i - 1])
+            while rk + 1 < n and s[rk + 1] not in occ:
+                # 不断地移动右指针
+                occ.add(s[rk + 1])
+                rk += 1
+            # 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = max(ans, rk - i + 1)
+        return ans
 
 
 if __name__ == '__main__':
-    pass
+    s1 = 'abcabcbb'
+    s2 = 'bbbbb'
+    s3 = 'pwwkew'
+    s4 = ''
+    s = Solution()
+    ret1 = s.lengthOfLongestSubstring(s1)
+    ret2 = s.lengthOfLongestSubstring(s2)
+    ret3 = s.lengthOfLongestSubstring(s3)
+    ret4 = s.lengthOfLongestSubstring(s4)
+    print(ret1)
+    print(ret2)
+    print(ret3)
+    print(ret4)
