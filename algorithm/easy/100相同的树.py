bee7fe8fa851d3efa915ed1830831d6c1bf65079
Garner Wong <jnwang.321@163.com>
2021-07-03 12:01:19 +0800

    .

--- algorithm/easy/100相同的树.py
+++ algorithm/easy/100相同的树.py
@@ -32,20 +32,76 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        pass
+
+        # def travel_tree_data(root):
+        #     if root is None:
+        #         return
+        #     q = [root]
+        #     while q:
+        #         n = q.pop(0)
+        #         yield n.val
+        #         if n.left:
+        #             q.append(n.left)
+        #         if n.right:
+        #             q.append(n.right)
+        #
+        # return True if [i for i in travel_tree_data(p)] == [i for i in travel_tree_data(q)] else False
+
+        if not p and not q:
+            return True
+        if p and q:
+            if p.val == q.val:
+                return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return False
 
 
 def create_tree(data_list):
-    for
-    pass
+    root = None
+    for data in data_list:
+        nn = TreeNode(data)
+        if root is None:
+            root = nn
+        else:
+            q = [root]
+            while q:
+                n = q.pop(0)
+                if n.left is None:
+                    n.left = nn
+                    continue
+                if n.right is None:
+                    n.right = nn
+                    continue
+                q.append(n.left)
+                q.append(n.right)
+    return root
 
 
-def travel_tree():
-    pass
+# def travel_tree(root):
+#     if root is None:
+#         return
+#     q = [root]
+#     while q:
+#         n = q.pop(0)
+#         yield n.val
+#         if n.left:
+#             q.append(n.left)
+#         if n.right:
+#             q.append(n.right)
 
 
 if __name__ == '__main__':
     p1, q1 = [1, 2, 3], [1, 2, 3]
     p2, q2 = [1, 2], [1, None, 2]
     p3, q3 = [1, 2, 1], [1, 1, 2]
-    pass
+    p1_root, q1_root = create_tree(p1), create_tree(q1)
+    # p1_list = [i for i in travel_tree(p1_root)]
+    # print(p1_list)
+    p2_root, q2_root = create_tree(p2), create_tree(q2)
+    p3_root, q3_root = create_tree(p3), create_tree(q3)
+    s = Solution()
+    ret1 = s.isSameTree(p1_root, q1_root)
+    ret2 = s.isSameTree(p2_root, q2_root)
+    ret3 = s.isSameTree(p3_root, q3_root)
+    print(ret1)
+    print(ret2)
+    print(ret3)
---
+++ algorithm/easy/94二叉树的中序遍历.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+"""
+给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+
+示例 1：
+输入：root = [1,null,2,3]
+输出：[1,3,2]
+
+示例 2：
+输入：root = []
+输出：[]
+
+示例 3：
+输入：root = [1]
+输出：[1]
+
+示例 4：
+输入：root = [1,2]
+输出：[2,1]
+
+示例 5：
+输入：root = [1,null,2]
+输出：[1,2]
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+
+
+if __name__ == '__main__':
+    pass
