bba1a5b2179a08f7de99a97d559fc587406f71ff
wjngit <jnwang.321@gmail.com>
2022-04-08 22:12:26 +0800

    learn 11-1

--- learn/11-1/8.py
+++ learn/11-1/8.py
@@ -31,20 +31,23 @@
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        n = len(word1)
-        m = len(word2)
-        dp = [[0] * (m + 1) for _ in range(n + 1)]
-        for j in range(m + 1):
-            dp[0][j] = j
-        for i in range(n + 1):
-            dp[i][0] = i
-        for i in range(1, n + 1):
-            for j in range(1, m + 1):
-                if word1[i - 1] == word2[j - 1]:
-                    dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1])
-                else:
-                    dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1)
-        return dp[n][m]
+        # m, n = len(word1), len(word2)
+        # dp = [[float("inf")] * (n + 1) for _ in range(m + 1)]
+        # for i in range(m + 1):
+        #     dp[i][0] = i
+        # for j in range(n + 1):
+        #     dp[0][j] = j
+        # for i in range(1, m + 1):
+        #     for j in range(1, n + 1):
+        #         if word1[i - 1] == word2[j - 1]:
+        #             dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1])
+        #         else:
+        #             dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1)
+        # if dp[m][n] == float("inf"):
+        #     return -1
+        # return dp[m][n]
+
+        pass
 
 
 if __name__ == '__main__':
--- learn/11-1/9.py
+++ learn/11-1/9.py
@@ -44,40 +44,42 @@ class Solution:
 
         pass
 
-        n = len(nums)
-        listToMinV = [float("inf")] * (n + 1)
-        k = 0
-        dp = [0] * n
-        for i in range(n):
-            length = self.bsearch(listToMinV, k, nums[i])
-            if length == -1:
-                dp[i] = 1
-            else:
-                dp[i] = length + 1
-            if dp[i] > k:
-                k = dp[i]
-                listToMinV[dp[i]] = nums[i]
-            elif listToMinV[dp[i]] > nums[i]:
-                listToMinV[dp[i]] = nums[i]
-        result = 0
-        for i in range(n):
-            if dp[i] > result:
-                result = dp[i]
-        return result
+        #     n = len(nums)
+        #     listToMinV = [float("inf")] * (n + 1)
+        #     k = 0
+        #     dp = [0] * n
+        #     for i in range(n):
+        #         length = self.bsearch(listToMinV, k, nums[i])
+        #         if length == -1:
+        #             dp[i] = 1
+        #         else:
+        #             dp[i] = length + 1
+        #         if dp[i] > k:
+        #             k = dp[i]
+        #             listToMinV[dp[i]] = nums[i]
+        #         elif listToMinV[dp[i]] > nums[i]:
+        #             listToMinV[dp[i]] = nums[i]
+        #     result = 0
+        #     for i in range(n):
+        #         if dp[i] > result:
+        #             result = dp[i]
+        #     return result
+        #
+        # def bsearch(self, a, k, target):
+        #     low = 1
+        #     high = k
+        #     while low <= high:
+        #         mid = (low + high) // 2
+        #         if a[mid] < target:
+        #             if mid == k or a[mid + 1] >= target:
+        #                 return mid
+        #             else:
+        #                 low = mid + 1
+        #         else:
+        #             high = mid - 1
+        #     return -1
 
-    def bsearch(self, a, k, target):
-        low = 1
-        high = k
-        while low <= high:
-            mid = (low + high) // 2
-            if a[mid] < target:
-                if mid == k or a[mid + 1] >= target:
-                    return mid
-                else:
-                    low = mid + 1
-            else:
-                high = mid - 1
-        return -1
+        pass
 
 
 if __name__ == '__main__':
